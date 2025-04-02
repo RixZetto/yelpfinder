a9ddae3960a1da53de42994d42f33dcc14e94fe2
Ricardo Rodriguez <rrodriguezgarcia@gmail.com>
2025-04-02 15:49:28 -0500

    Initial Commit

---
+++ YelpFinderTests/YelpFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  YelpFinderTests.swift
+//  YelpFinderTests
+//
+//  Created by Ricardo Rodríguez on 2/04/25.
+//
+
+import Testing
+@testable import YelpFinder
+
+struct YelpFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
